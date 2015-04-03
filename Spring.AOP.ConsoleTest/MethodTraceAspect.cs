ce5ccf7de066b79069735bfcf5a848bb3e2b2a27
jefflord <jeff@jefflord.com>
2015-04-03 17:39:28 -0400

    Missing file?

---
+++ Spring.AOP.ConsoleTest/MethodTraceAspect.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PostSharp.Aspects;
+
+namespace Spring.AOP.ConsoleTest
+{
+    [Serializable]
+    public class MethodTraceAspect : OnMethodBoundaryAspect
+    {
+        public override void OnEntry(MethodExecutionArgs args)
+        {
+            //args.MethodExecutionTag = Stopwatch.StartNew();
+            return;
+        }
+
+        //public override void OnExit(MethodExecutionArgs args)
+        //{
+        //    Stopwatch sw = (Stopwatch)args.MethodExecutionTag;
+        //    sw.Stop();            
+        //}
+    }
+}
